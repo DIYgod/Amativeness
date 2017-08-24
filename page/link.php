5212f2f884d194d6d1e9853f50f8ccb00456f46f
DIYgod <diy.d.god@gmail.com>
2017-08-25 00:13:09 +0800

    special link

--- page/link.php
+++ page/link.php
@@ -6,18 +6,18 @@ get_header(); ?>
 <div class="main">
     <?php while ( have_posts() ) : the_post(); ?>
         <article id="post-<?php the_ID(); ?>" class="block post linkpage">
-            <h3>DIYgod 相关</h3>
+            <h3>超级棒</h3>
             <ul>
                 <?php
                 $bookmarks = get_bookmarks( array(
                     'orderby'        => 'name',
                     'order'          => 'ASC',
-                    'category_name'  => 'me'
+                    'category_name'  => 'special'
                 ));
 
                 // Loop through each bookmark and print formatted output
                 foreach ( $bookmarks as $bookmark ) {
-                    printf( '<li><a href="%s" target="_blank"><img src="%s"><h4>%s</h4><p>%s</p></a></li>', $bookmark->link_url, $bookmark->link_image ? $bookmark->link_image : 'https://linklink.b0.upaiyun.com/linkdefault.jpg', $bookmark->link_name, $bookmark->link_description ? $bookmark->link_description : '这家伙很懒，什么也没有留下' );
+                    printf( '<li><a href="%s" target="_blank" rel="follow"><img src="%s"><h4>%s</h4><p>%s</p></a></li>', $bookmark->link_url, $bookmark->link_image ? $bookmark->link_image : 'https://linklink.b0.upaiyun.com/linkdefault.jpg', $bookmark->link_name, $bookmark->link_description ? $bookmark->link_description : '这家伙很懒，什么也没有留下' );
                 }
                 ?>
             </ul>
@@ -36,7 +36,7 @@ get_header(); ?>
                 }
                 ?>
             </ul>
-            <h3>貌似死掉了的小伙伴(复活后请及时与我联系)</h3>
+            <h3>貌似挂掉了</h3>
             <ul>
                 <?php
                 $bookmarks = get_bookmarks( array(
